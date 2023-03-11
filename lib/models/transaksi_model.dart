07b8b74f3ea4f1dbb4abf7e8bec1995bfdbef812
widiasihrambee <widiasihrambe@gmail.com>
2023-03-11 12:47:12 +0700

    1

--- lib/models/transaksi_model.dart
+++ lib/models/transaksi_model.dart
@@ -1,27 +1,27 @@
-// class TransaksiModel {
-//   /*
-//   tipe
-//   1 -> pemasukan
-//   2 -> pengeluaran
-//   */
-//   int? id, type, total;
-//   String? name, createdAt, updatedAt;
+class TransaksiModel {
+  /*
+  tipe
+  1 -> pemasukan
+  2 -> pengeluaran
+  */
+  int? id, type, total;
+  String? name, createdAt, updatedAt;
 
-//   TransaksiModel(
-//       {this.id,
-//       this.type,
-//       this.total,
-//       this.name,
-//       this.createdAt,
-//       this.updatedAt});
+  TransaksiModel(
+      {this.id,
+      this.type,
+      this.total,
+      this.name,
+      this.createdAt,
+      this.updatedAt});
 
-//   factory TransaksiModel.fromJson(Map<String, dynamic> json) {
-//     return TransaksiModel(
-//         id: json['id'],
-//         type: json['type'],
-//         total: json['total'],
-//         name: json['name'],
-//         createdAt: json['created_at'],
-//         updatedAt: json['updated_at']);
-//   }
-// }
+  factory TransaksiModel.fromJson(Map<String, dynamic> json) {
+    return TransaksiModel(
+        id: json['id'],
+        type: json['type'],
+        total: json['total'],
+        name: json['name'],
+        createdAt: json['created_at'],
+        updatedAt: json['updated_at']);
+  }
+}
